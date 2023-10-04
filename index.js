050f7248f838af9f1b2f6034cd1a2ab29aa473a7
st-quy <quy.pham@stunited.vn>
2023-10-04 15:24:13 +0700

    done

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
+let tbody = document.getElementById("tbody");
 
-let tbody = document.getElementById("tbody")
-
-
-
-// fetch function
 fetch("https://tour-booking.glitch.me/user")
-    .then(res => res.json())
-    .then(json => {
-        json.map(data => {
-            console.log(data)
-            tbody.append(td_fun(data));
-        })
-    })
+  .then((res) => res.json())
+  .then((json) => {
+    json.map((data) => {
+      tbody.append(td_fun(data));
+    });
+  });
 
-// create td
-function td_fun({ profile, name, email, status, role}){
-    let td = document.createElement('tr');
-    td.innerHTML = `
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+var modal = document.getElementById("id01");
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function td_fun({ id, profile, name, email, status, role }) {
+  let td = document.createElement("tr");
+  td.setAttribute("id", `child-${id}`);
+  td.innerHTML = `
     <td class="px-6 py-4 whitespace-nowrap">
         <div class="flex items-center">
                 <div class="flex-shrink-0 h-10 w-10">
@@ -40,6 +55,43 @@ function td_fun({ profile, name, email, status, role}){
     <td class="px-6 py-4 whitespace-nowrap">
         <span class="text-sm text-gray-500">${role}</span>
     </td>
+ <td class="px-6 py-4 whitespace-nowrap">
+<button class="text-sm text-gray-500" id="deleteBtn" onclick="handleDelete(${id})">Delete</button>
+    </td>
     `;
-    return td;
+  return td;
+}
+
+async function handleDelete(id) {
+  event.preventDefault();
+
+  await axios
+    .delete(`https://tour-booking.glitch.me/user/${id}`)
+    .then((response) => {
+      document.getElementById(`child-${id}`).remove();
+    });
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  var name = document.querySelector('input[name="name"]').value;
+  var email = document.querySelector('input[name="email"]').value;
+  var profile = document.querySelector('input[name="profile"]').value;
+  var status = document.querySelector('input[name="status"]').value;
+  var role = document.querySelector('input[name="role"]').value;
+  await axios
+    .post("https://tour-booking.glitch.me/user", {
+      name: name,
+      email: email,
+      profile: profile,
+      status: status,
+      role: role,
+    })
+    .then((response) => {
+      var modal = document.getElementById("id01");
+      modal.style.display = "none";
+      tbody.append(td_fun(response.data));
+    });
+  event.target.reset();
 }
